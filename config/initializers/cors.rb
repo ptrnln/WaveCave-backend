4f94ab984615c4d4a44b031b4a051f81cce8ca8b
peter <peter.l.nolan@gmail.com>
2024-11-09 21:34:49 -0500

    CORS stuff

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,11 +10,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     if Rails.env.production?
       origins "https://ph4se.dev", "https://www.ph4se.dev", "http://127.0.0.1*", "http://localhost:*"
       resource "/session/",
-        headers: ['Content-Type'],
+        headers: ['Content-Type', 'X-CSRF-Token', 'X-Csrf-Token'],
         methods: [ :get, :post, :delete, :options ],
         credentials: true
       resource "*",
-        headers: ['Content-Type'],
+        headers: ['Content-Type', 'X-CSRF-Token', 'X-Csrf-Token'],
         methods: [:get, :post, :put, :patch, :delete, :head],
         credentials: true
     else
